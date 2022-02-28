3c6aaac48c246e7c5af34800f3df8bc04aee6fe5
Ananias Raphael <ananias.nobrega@gmail.com>
2022-02-27 23:07:36 -0300

    add view

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set('view engeni', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(8000, function(){
+    console.log('server start on port 8000');
+})
