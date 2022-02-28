365222d24ea2569e241b5eda3e19b993b4a0e92e
Ananias Raphael <ananias.nobrega@gmail.com>
2022-02-28 11:10:54 -0300

    fix and scren question

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.set('view engeni', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/perguntar', (req, res) => {
+app.get('/question', (req, res) => {
     res.render('question');
 });
 
-app.listen(8000, ()=>{
+app.listen(8000, () => {
     console.log('server start on port 8000');
 })
